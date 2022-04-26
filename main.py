d3b1bba22c69537c6598be4954b91638523dc4c8
Juan Elian Gluschancoff <juan.gluschancoff@globalprocessing.com.ar>
2022-04-26 18:03:54 -0300

    calculadora sin terminar

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from json.tool import main
+from principalFuntions import sumar
+
+main
+
 print ("BIENVENIDO A LA CALCULADORA")
+print ("ELIJA LA OPCION QUE NECESITE: ")
+
+
 print ("ELIJA LA OPCION QUE NECESITE: ")
 print ("1.SUMAR")
 print ("2.RESTAR")
 print ("3.DIVIDIR")
 print ("4.MULTIPLICA")
+
+
+opcionSeleccionadaMenu = input()
+
+if opcionSeleccionadaMenu == 1:
+    resultadoSuma = sumar(numero1, numero2)
+    print("el resultado de la suma es: " + resultadoSuma)
+
+
 
---
+++ principalFuntions.py
@@ -0,0 +1,14 @@
+def sumar(numero1, numero2):
+    resultadoSuma = numero1 + numero2
+    return resultadoSuma
+
+def restar(numero1, numero2):
+    return numero1 + numero2
+
+def multiplicar(numero1, numero2):
+    return numero1 * numero2
+
+def dividir(numero1, numero2):
+    resultadoDivisionReal = numero1 / numero2
+    resultadoDivisionResto = numero1 % numero2 
+    return "el resultado de la division es:" + resultadoDivisionReal + " y el resultado del resto es: " + resultadoDivisionResto
